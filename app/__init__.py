deaae15951f0fab5be01d0fcb959fc2946bddf66
Allan Assis <allan-bdc@hotmail.com>
2018-11-30 00:50:19 -0200

    Add put route to update documents

--- app/__init__.py
+++ app/__init__.py
@@ -8,14 +8,26 @@ es = Elastic("localhost", 9200)
 
 @app.route("/", methods=["GET"])
 def get():
-    import pdb
-    pdb.set_trace()
-    identifier = json.loads(request.data)
-    return json.dumps(es.getDoc(identifier["id"]))
+    if not len(request.args):
+        return json.dumps(es.get_all_doc())
+    identifier = request.args['id']
+    return json.dumps(es.get_doc(identifier))
 
 
 @app.route("/", methods=["POST"])
 def post():
     body = json.loads(request.data)
-    document = es.insert(body)
+    document = es.insert_doc(body)
+    return json.dumps(document)
+
+
+@app.route("/", methods=["PUT"])
+def put():
+    if not len(request.args):
+        return "You must specify the id"
+    if not len(request.data):
+        return "Not modified"
+    body = {"doc": json.loads(request.data)}
+    identifier = request.args['id']
+    document = es.update_doc(identifier, body)
     return json.dumps(document)
