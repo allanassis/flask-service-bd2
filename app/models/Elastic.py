8dcbfe51fff3e9524f842b0420efa52c98afff5c
Allan Assis <allan-bdc@hotmail.com>
2018-11-30 00:52:10 -0200

    Refactoring Elastic model
    Add update method and get all documents

--- app/models/Elastic.py
+++ app/models/Elastic.py
@@ -9,10 +9,18 @@ class Elastic(Elasticsearch):
     def __init__(self, domain, port):
         Elasticsearch.__init__(self, [{'host': domain, 'port': port}])
 
-    def getDoc(self, ident, query=None):
-        res = self.get(settings.index, settings.doc_type, ident, query)
-        return res
+    def get_doc(self, identifier):
+        try:
+            return self.get(settings.index, settings.doc_type, id=identifier)
+        except:
+            return "Document not found"
 
-    def insert(self, document):
+    def get_all_doc(self):
+        return self.search(index=settings.index, body={"query": {"match_all": {}}})
+
+    def insert_doc(self, document):
         res = self.index(settings.index, settings.doc_type, document)
         return res
+
+    def update_doc(self, identifier, document):
+        return self.update(settings.index, settings.doc_type, id=identifier, body=document)
